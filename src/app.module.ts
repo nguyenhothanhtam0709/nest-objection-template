a34982501c12543842792a79b71ea559a6af0d05
TamNHT <nguyenhothanhtam0709@gmail.com>
2022-12-30 20:45:06 +0700

    feat: add simple logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { registerConfigModule } from '@start/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +6,6 @@ import { AppService } from './app.service';
 @Module({
   imports: [registerConfigModule()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Logger],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { EnvVarName } from '@enums/env';
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -6,8 +7,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const logger = app.get(Logger);
 
   const port = configService.get<number>(EnvVarName.API_PORT);
   await app.listen(port);
+  logger.log(`Server is listening on port ${port}`);
 }
 bootstrap();
